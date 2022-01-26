532d05364b2c49bad77c004a90d4be547105c48d
Nicolò Bartolini <nicolobartolinii@gmail.com>
2022-01-26 11:39:12 +0100

    Script del gioco finito.

--- script.js
+++ script.js
@@ -12,14 +12,36 @@ function computerPlay() {
 
 function round(playerSelection, cpuSelection) {
     if (playerSelection.toUpperCase() === cpuSelection.toUpperCase()) {
-        return "Pareggio";
+        return 0;
     }
     else if ((playerSelection.toUpperCase() === "CARTA" && cpuSelection === "Sasso") || (playerSelection.toUpperCase() === "FORBICE" && cpuSelection === "Carta") || (playerSelection.toUpperCase() === "SASSO" && cpuSelection === "Forbice"))
-        return `Giocatore vince. ${playerSelection} batte ${cpuSelection}!`;
+        return 1;
     else
-        return `Computer vince. ${cpuSelection} batte ${playerSelection}!`;
+        return 2;
 }
 
-let scelta = prompt("Scelta");
+function game() {
+    let counterPlayer = 0;
+    let counterCPU = 0;
+    for (let i = 1; i <= 5;) {
+        let scelta = prompt(`Round ${i}. Scelta.`);
+        let risultato = round(scelta, computerPlay());
+        if (risultato === 0);
+        else if (risultato === 1) {
+            counterPlayer++;
+            i++;
+        }
+        else if (risultato === 2) {
+            counterCPU++;
+            i++;
+        }
+    }
 
-console.log(round(scelta, computerPlay()));
+    if (counterPlayer > counterCPU) {
+        return `Giocatore vince. ${counterPlayer} - ${counterCPU}`
+    }
+    else
+        return `Computer vince. ${counterPlayer} - ${counterCPU}`
+}
+
+console.log(game());
